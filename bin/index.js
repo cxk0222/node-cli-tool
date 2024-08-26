004953cd06635d448d458b83a04bfc1dd7840303
caoxiaokang <caoxiaokang@xinye.com>
2024-08-26 15:34:48 +0800

    add arg dependency

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,22 @@
 #!/usr/bin/env node
-console.log('hello tool')
+const arg = require('arg')
+
+try {
+  const args = arg({
+    '--start': Boolean,
+    '--build': Boolean,
+  });
+
+  if (args['--start']) {
+    console.log('starting the app')
+  }
+} catch (e) {
+  console.log(e.message)
+  usage()
+}
+
+function usage() {
+  console.log(`tool [CMD]
+  --start\tStarts the app
+  --build\tBuilds the app`)
+}
